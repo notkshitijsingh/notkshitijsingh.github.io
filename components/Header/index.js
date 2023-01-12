e6bb46d03ddf9a96233fff2a0919bbc4726cd1db
Kshitij Singh <chaharkshitij@gmail.com>
2023-01-13 04:02:08 +0530

    Update index.js

--- components/Header/index.js
+++ components/Header/index.js
@@ -108,7 +108,7 @@ const Header = ({ handleWorkScroll, handleAboutScroll, isBlog }) => {
                   )}
 
                   <Button
-                    onClick={() => window.open("mailto:hello@chetanverma.com")}
+                    onClick={() => window.open("mailto:chaharkshitij@gmail.com")}
                   >
                     Contact
                   </Button>
